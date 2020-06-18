fb6c85d87f0265ccfc08a13fcb0411164f339ffb
Mdsomratakbor <somrat876@gmail.com>
2020-06-19 01:16:34 +0600

    WorkReport class add

--- SRP-Demo-WorkReport/Program.cs
+++ SRP-Demo-WorkReport/Program.cs
@@ -10,6 +10,14 @@ namespace SRP_Demo_WorkReport
     {
         static void Main(string[] args)
         {
+            var report = new WorkReport();
+            report.AddEntry(new WorkReportEntry { ProjectCode = "123Ds", ProjectName = "Project1", SpentHours = 5 });
+            report.AddEntry(new WorkReportEntry { ProjectCode = "987Fc", ProjectName = "Project2", SpentHours = 3 });
+
+            Console.WriteLine(report.ToString());
+
+            var saver = new FileSaver();
+            saver.SaveToFile(@"Reports", "WorkReport.txt", report);
         }
     }
 }
--- SRP-Demo-WorkReport/WorkReportEntry.cs
+++ SRP-Demo-WorkReport/WorkReportEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,4 +13,18 @@ namespace SRP_Demo_WorkReport
         public string ProjectName { get; set; }
         public int SpentHours { get; set; }
     }
+
+//public class FileSaver
+//    {
+//        public void SaveToFile(string directoryPath, string fileName, WorkReport report)
+//        {
+//            if (!Directory.Exists(directoryPath))
+//            {
+//                Directory.CreateDirectory(directoryPath);
+//            }
+
+//            File.WriteAllText(Path.Combine(directoryPath, fileName), report.ToString());
+//        }
+//    }
+
 }
