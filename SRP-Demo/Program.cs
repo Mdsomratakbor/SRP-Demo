054c13e438e6ba6e220bce310ab25c56e2562045
Mdsomratakbor <somrat876@gmail.com>
2020-06-18 22:31:06 +0600

    StandardMessage class add

--- SRP-Demo/Program.cs
+++ SRP-Demo/Program.cs
@@ -18,21 +18,21 @@ namespace SRP_Demo
             Console.Write("What is you Last name :");
             user.LastName = Console.ReadLine();
             //Checks to be sure the first and last name are valid
-            if (string.IsNullOrWhiteSpace(user.FirstName))
+            if (!string.IsNullOrWhiteSpace(user.FirstName))
             {
                 Console.WriteLine("Yod did not give us a valid first name !");
                 Console.ReadLine();
                 return;
             }
-            if (string.IsNullOrWhiteSpace(user.LastName))
+            if (!string.IsNullOrWhiteSpace(user.LastName))
             {
                 Console.WriteLine("Yod did not give us a valid last name !");
                 Console.ReadLine();
                 return;
             }
             // create a username for the person
-            Console.WriteLine($"Your username is {user.FirstName.Substring(0,1)}{user.LastName}")
-
+            Console.WriteLine($"Your username is {user.FirstName.Substring(0, 1)}{user.LastName}");
+            Console.ReadLine();
         }
     }
 }
